543ae473c60d530f86e1bb67f34e35042197ee56
AMINE IDELHAJ <84861819+amineidel1@users.noreply.github.com>
2023-12-16 19:18:15 +0100

    Delete AddicTest_APP directory

--- AddicTest_APP/predict/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, about, simulation, resultat
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('about', about, name='about'),
-    path('simulation', simulation, name='simulation'),
-    path('resultat', resultat, name='resultat'),
-]
